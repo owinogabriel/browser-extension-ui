d4283ee5a7cfed84a953d09a9bb8b87c35b9404e
owinogabriel <g73346920@gmail.com>
2025-09-29 22:24:53 +0300

    feat:import header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <Header />
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
