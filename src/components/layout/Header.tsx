2c23305ff1f4790044137501d658e0f343e4b605
owinogabriel <g73346920@gmail.com>
2025-09-30 04:12:13 +0300

    feat:update the implemented Ui

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,25 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { ThemeToggle } from "./ThemeToggle";
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-4 h-16 flex justify-between items-center">
-        <Link href="/"></Link>
-        <Image
-          src="/assets/images/logo.svg"
-          alt="logo"
-          width={150}
-          height={40}
-        />
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={150}
+            height={40}
+          />
+        </Link>
+        <div className="flex items-center space-x-4">
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
