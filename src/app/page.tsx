a0e0bd04896dc241b924947a0e77e8c57cd16d7b
owinogabriel <g73346920@gmail.com>
2025-10-01 11:05:05 +0300

    chore:modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+import { Button } from "@/components/ui/Button";
+
 export default function Home() {
+  const [activeState, setActiveState] = "All";
+  const filters = ["All", "Active", "Inactive"];
+
   return (
     <div className="container mx-auto px-4 py-8 ">
-      <div className="text-center mb-8">
-        <h1 className="text-4xl md:text-6xl font-bold">Browser Extension</h1>
-        <p className="text-xl text-muted-foreground max-w-2xl mt-2 mx-auto">
-          Discover, Search, and collect your favorite Browser Extension with our
-          comprehensive explorer.
-        </p>
+      <div className="flex justify-between mb-8">
+        <h1 className="text-4xl  font-bold">Browser Extension</h1>
+        <div className="flex gap-4">
+          {filters.map((filter) => (
+            <Button
+            size="sm"
+              key={filter}
+              variant={activeState === filter ? "default" : "outline"}
+              className={
+                activeState === filter
+                  ? "bg-orange-500 text-white hover:bg-orange-600"
+                  : ""
+              }
+              onClick={() => setActiveState(filter)}
+            >
+               {filter.charAt(0).toUpperCase() + filter.slice(1)}
+            </Button>
+          ))}
+        </div>
       </div>
     </div>
   );
